7b9cd5c4a8dea825c8fa690a2bfb8382dd62b73b
Guilherme <Alves.fernando1@aluno.ifsp.edu.br>
2025-03-26 17:41:44 -0300

    aquario

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,40 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { FormsModule } from '@angular/forms';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonInput, IonItem, IonList, IonButton  } from '@ionic/angular/standalone';
+
+
+
+interface Task {
+  id: number;
+  nome: string;
+  isFeita: boolean;
+}
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonInput, IonItem, IonList, CommonModule, FormsModule, IonButton ],
 })
 export class HomePage {
+
+  novaTask: string = ''
+
+tasks: Task[] = []
+
   constructor() {}
+
+  addTask() {
+
+    const obj:Task = {
+      id: Date.now(),
+      nome: this.novaTask,
+      isFeita: false
+    }
+
+    this.tasks.push(obj)
+    console.log(this.tasks);
+    
+  }
 }
