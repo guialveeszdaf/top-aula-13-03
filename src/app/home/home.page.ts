b64b34d34210da60020cded48e5c1c5579b89115
Guilherme Alves <alves.fernando1@aluno.ifsp.edu.br>
2025-04-07 12:42:08 -0300

    trabalho

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,40 +1,53 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { IonHeader, IonToolbar, IonTitle, IonContent, IonInput, IonItem, IonList, IonButton  } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonInput, IonItem, IonList, IonButton, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle  } from '@ionic/angular/standalone';
 
 
 
-interface Task {
-  id: number;
-  nome: string;
-  isFeita: boolean;
+interface Porta {
+  readonly id: number;
+  premio: string
 }
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonInput, IonItem, IonList, CommonModule, FormsModule, IonButton ],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonInput, IonItem, IonList, CommonModule, FormsModule, IonButton, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle ],
 })
 export class HomePage {
 
-  novaTask: string = ''
-
-tasks: Task[] = []
+  portas: Porta[] = []
 
   constructor() {}
 
-  addTask() {
+  porta1() {
 
-    const obj:Task = {
-      id: Date.now(),
-      nome: this.novaTask,
-      isFeita: false
+    const premio1:Porta = {
+      id: 1,
+    premio: 'cabra'
     }
 
-    this.tasks.push(obj)
-    console.log(this.tasks);
+    this.portas.push(premio1)
+    console.log(this.porta1);
     
   }
+
+  porta2() {
+
+    const premio2:Porta = {
+      id: 2,
+      premio: 'carro'
+    }
+  }
+
+  porta3() {
+
+    const premio3:Porta = {
+      id: 3,
+      premio: 'cabra'
+    }
+  }
 }
+
